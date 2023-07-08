ba69c4f43e20f74fa5050fdd6a620e5c3b287b38
Mikhail220220 <135328072+Mikhail220220@users.noreply.github.com>
2023-07-09 00:15:07 +0600

    finish1

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Entrer is numbers: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void NumbersToN (int n){
+    if (n>1) NumbersToN (n-1);
+    Console.Write (n + " ");
+
+}
+NumbersToN(n);
